62755d32023c1624d391b2969f6d39879c831520
NiuBoss123 <niuboss123@foxmail.com>
2025-03-15 02:11:35 +0800

    增加评论系统

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -160,17 +160,17 @@ export default defineUserConfig({
        * 评论 comments
        * @see https://theme-plume.vuejs.press/guide/features/comments/
        */
-      // comment: {
-      //   provider: '', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
-      //   comment: true,
-      //   repo: '',
-      //   repoId: '',
-      //   category: '',
-      //   categoryId: '',
-      //   mapping: 'pathname',
-      //   reactionsEnabled: true,
-      //   inputPosition: 'top',
-      // },
+      comment: {
+        provider: 'Giscus', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
+        comment: true,
+        repo: 'NiuBoss123/Akio-Plume',
+        repoId: 'R_kgDON11zOA',
+        category: 'blog',
+        categoryId: 'DIC_kwDON11zOM4CoCQO',
+        mapping: 'pathname',
+        reactionsEnabled: true,
+        inputPosition: 'top',
+      },
     },
 
     /**
